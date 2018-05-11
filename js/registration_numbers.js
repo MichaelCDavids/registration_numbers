ac85da350baad44f7797bd1056c19f5489559087
michael <michaelcdavids@gmail.com>
2018-05-11 12:59:36 +0200

    initial commit

---
+++ js/registration_numbers.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded',function(){
+  //Getting references to all elements
+  var input = document.querySelector('.registration-number-field');
+  var addBtn = document.querySelector('.add-button');
+  var output = document.querySelector('.registration_number');
+
+  
+});
