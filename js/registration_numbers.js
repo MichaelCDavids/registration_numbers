e51eddd01664f1916f0a0621f34666d48ec12de9
michael <michaelcdavids@gmail.com>
2018-05-12 12:45:07 +0200

    made items be added dynamically

--- js/registration_numbers.js
+++ js/registration_numbers.js
@@ -2,7 +2,16 @@ document.addEventListener('DOMContentLoaded',function(){
   //Getting references to all elements
   var input = document.querySelector('.registration-number-field');
   var addBtn = document.querySelector('.add-button');
-  var output = document.querySelector('.registration_number');
+  var output = document.querySelector('.registration_list');
 
-  
+  addBtn.addEventListener('click',function(){
+    var enteredRegNumber = input.value
+    var newListItem = document.createElement('li');
+    newListItem.textContent = enteredRegNumber;
+    //var someContent = document.createTextNode(enteredRegNumber);
+    output.appendChild(newListItem);
+    input.value = "";
+    //var currentItem = document.getElementById("main-list");
+    //document.body.insertBefore(newListItem,currentItem);
+  });
 });
