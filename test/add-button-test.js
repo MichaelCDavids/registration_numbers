c6708f2670b5385ad5c97b9ac48db3a620e66eef
michael <michaelcdavids@gmail.com>
2018-05-17 10:01:05 +0200

    added more test for the add function and filter function

--- test/add-button-test.js
+++ test/add-button-test.js
@@ -10,4 +10,29 @@ describe('The addRegNumber Function',function(){
     registrationNumbersObject.addReg('CY 253 158');
     assert.equal(registrationNumbersObject.lister(registrationNumbersObject.theMap()).length,6);
   });
+  it('should return the correct amount of registration numbers that have been captured if duplicates have been entered',function(){
+
+    var regNumObj = RegistrationFactoryFunction();
+
+    regNumObj.addReg('CA 123 456');
+    regNumObj.addReg('CY 213 546');
+    regNumObj.addReg('CJ 264 128');
+    regNumObj.addReg('CA 123 556');
+    regNumObj.addReg('CJ 513 546');
+    regNumObj.addReg('CY 253 158');
+    regNumObj.addReg('CJ 523 576');
+    regNumObj.addReg('CJ 533 566');
+    regNumObj.addReg('CJ 543 566');
+    regNumObj.addReg('CA 123 456');
+    regNumObj.addReg('CY 213 546');
+    regNumObj.addReg('CJ 264 128');
+    regNumObj.addReg('CA 123 556');
+    regNumObj.addReg('CJ 513 546');
+    regNumObj.addReg('CY 253 158');
+    regNumObj.addReg('CJ 523 576');
+    regNumObj.addReg('CJ 533 566');
+    regNumObj.addReg('CJ 543 566');
+
+      assert.equal(regNumObj.lister(regNumObj.theMap()).length,9);
+  });
 });
--- test/filter-function-test.js
+++ test/filter-function-test.js
@@ -1,6 +1,6 @@
 describe('The Filter Function',function(){
 
-  it('should return how many registration numbers are for the selected town',function(){
+  it('should return how many registration numbers are for the Cape Town',function(){
 
     var regNumObj = RegistrationFactoryFunction();
 
@@ -11,6 +11,35 @@ describe('The Filter Function',function(){
     regNumObj.addReg('CJ 513 546');
     regNumObj.addReg('CY 253 158');
 
-      assert.equal(regNumObj.filter(regNumObj.lister(regNumObj.theMap()),'CA'),regNumObj.filter(regNumObj.lister(regNumObj.theMap()),'CA'));
+      assert.equal(regNumObj.filter(regNumObj.lister(regNumObj.theMap()),'CA').length,2);
+  });
+  it('should return how many registration numbers are for Bellville',function(){
+
+    var regNumObj = RegistrationFactoryFunction();
+
+    regNumObj.addReg('CA 123 456');
+    regNumObj.addReg('CY 213 546');
+    regNumObj.addReg('CJ 264 128');
+    regNumObj.addReg('CA 123 556');
+    regNumObj.addReg('CJ 513 546');
+    regNumObj.addReg('CY 253 158');
+
+      assert.equal(regNumObj.filter(regNumObj.lister(regNumObj.theMap()),'CY').length,2);
+  });
+  it('should return how many registration numbers are for Paarl',function(){
+
+    var regNumObj = RegistrationFactoryFunction();
+
+    regNumObj.addReg('CA 123 456');
+    regNumObj.addReg('CY 213 546');
+    regNumObj.addReg('CJ 264 128');
+    regNumObj.addReg('CA 123 556');
+    regNumObj.addReg('CJ 513 546');
+    regNumObj.addReg('CY 253 158');
+    regNumObj.addReg('CJ 523 576');
+    regNumObj.addReg('CJ 533 566');
+    regNumObj.addReg('CJ 543 566');
+
+      assert.equal(regNumObj.filter(regNumObj.lister(regNumObj.theMap()),'CJ').length,5);
   });
 });
